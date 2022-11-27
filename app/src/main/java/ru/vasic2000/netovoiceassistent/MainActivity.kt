53a38e23fff3810200858943ea432f3ea4540385
Vasic2000 <vasic2000@yandex.ru>
2022-11-27 15:38:42 +0300

    ToolBar menu

--- app/src/main/java/ru/vasic2000/netovoiceassistent/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/netovoiceassistent/MainActivity.kt
@@ -2,10 +2,43 @@ package ru.vasic2000.netovoiceassistent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val m_toolbar : MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(m_toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
